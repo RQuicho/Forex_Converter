7b81d689fae73c86d3700090f5448497bfcd0223
RQuicho <raymondquicho@gmail.com>
2023-05-08 22:43:36 -0700

    completing basic functionality for converting currencies using the api

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import requests
+import json
 
-url = 'https://api.exchangerate.host/convert?from=USD&to=EUR'
-response = requests.get(url)
-data = response.json()
-
-print(data)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'using forex to convert currency'
@@ -13,5 +9,28 @@ app.config['SECRET_KEY'] = 'using forex to convert currency'
 @app.route('/')
 def home_page():
 	"""Shows home page with form inputs."""
-	from_input = request.args['firstCurrency']
-	to_input = request.args['secondCurrency']
+	return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+	"""Converts currency"""
+	
+	first_curr = request.form['from']
+	second_curr = request.form['to']
+	amount = request.form['amount']
+	response = requests.get(f'https://api.exchangerate.host/convert?from={first_curr}&to={second_curr}&amount={amount}')
+	# response = requests.get('https://api.exchangerate.host/convert?from=USD&to=EUR&amount=100')
+	data = response.json()
+
+	# return data
+	return json.dumps(data['result'])
+
+	# 1 USD = 0.9100 EUR
+	# 100 USD = 91.00 EUR
+
+	# 1 USD = 134.97 JPY
+	# 100 USD = 13,495 JPY
+
+
+
+
---
+++ test_app.py
@@ -0,0 +1,20 @@
+from app import app
+
+class ForexTestCases(TestCase):
+	"""Tests all functions for converting currency"""
+
+	def test_home_page(self):
+		with app.test_client() as client:
+			resp = client.get('/')
+			html = resp.get_data(as_text=True)
+
+			self.assertEqual(resp.status_code, 200)
+			self.assertIn('<input type="text" name="from">', html)
+
+	def test_convert_submit(self):
+		with app.test_client() as client:
+			resp = client.post('/convert', data={'from': 'USD', 'to': 'EUR', 'amount': 100})
+			html = resp.get_data(as_text=True)
+
+			self.assertEqual(resp.status_code, 200)
+			self.assertEqual(resp.data['result'], 91.009788)
