22a420c4c58c8f1fc48efc83a84565f4ae88ffde
RQuicho <raymondquicho@gmail.com>
2023-08-17 08:40:26 -0700

    updating requirements.txt for certifi update

--- test_app.py
+++ test_app.py
@@ -26,9 +26,24 @@ class ForexTestCases(TestCase):
 			self.assertEqual(resp.status_code, 200)
 			self.assertIn('<h1>The result is $ 1</h1>', result)
 
-			self.assertEqual(resp.data['from'], 'USD')
-			self.assertEqual(resp.data['to'], 'USD')
-			self.assertIsInstance(resp.data['amount'], float)
+	def test_input_values(self):
+		with app.test_client() as client:
+			resp = client.post("/convert", data={
+				"from": "USD",
+				"to": "USD",
+				"amount": 1
+			})
+
+			data = resp.json()
+			result = json.dumps(data['result'])
+
+			self.assertEqual(result['from'], 'USD')
+
+			# self.assertEqual(resp.data['from'], 'USD')
+			# self.assertEqual(len(resp.data['from']), 3)
+			# self.assertEqual(resp.data['to'], 'USD')
+			# self.assertEqual(len(resp.data['to']), 3)
+			# self.assertIsInstance(resp.data['amount'], float)
 
 
 
